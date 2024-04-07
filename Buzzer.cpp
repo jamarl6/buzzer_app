8f705e0adb268c8f209f137ffbbb08b8e0ee480b
Marliani <jakob.marliani@gmx.de>
2024-04-07 17:13:32 +0200

    fixed syntax

--- Buzzer.cpp
+++ Buzzer.cpp
@@ -2,8 +2,8 @@
 #include <WiFiClient.h>
 #include <WiFiAP.h>
 
-const char *ssid = "GangBuzzer";
-const char *password = "TillistderBoss";
+const char* ssid = "GangBuzzer";
+const char* password = "TillistderBoss";
 unsigned long startTime = 0;
 
 bool running = false;
@@ -11,32 +11,32 @@ bool running = false;
 WiFiServer server(80);
 
 //3 LEDS
-int gameLEDS[] = {14,25,26};
+int gameLEDS[] = { 14,25,26 };
 //3 BUTTONS
-int gameBTNS[] = {15,5,4};
+int gameBTNS[] = { 15,5,4 };
 //PIEZO BUZZER 
 int buzzer = 13;
 //ALL DELAYS
-int buzzerDelay= 100;
-int resetDelay= 12000;
+int buzzerDelay = 100;
+int resetDelay = 12000;
 
 
 void setup() {
 
-//Serial begin allows the serial port to read and monitor functions
-      Serial.begin(115200);
-//This loops and sets up the LED pins to be output in an array
-      for (int i; i<3; i++){
-      pinMode(gameLEDS[i], OUTPUT);
-    }
-//This loops and sets up the Button pins to be INPUT_PULLUP in an array. Ground is HIGH so signal goes LOW on depress.
-      for (int i; i<3; i++){
-      pinMode(gameBTNS[i], INPUT_PULLUP);
-    }
+  //Serial begin allows the serial port to read and monitor functions
+  Serial.begin(115200);
+  //This loops and sets up the LED pins to be output in an array
+  for (int i; i < 3; i++) {
+    pinMode(gameLEDS[i], OUTPUT);
+  }
+  //This loops and sets up the Button pins to be INPUT_PULLUP in an array. Ground is HIGH so signal goes LOW on depress.
+  for (int i; i < 3; i++) {
+    pinMode(gameBTNS[i], INPUT_PULLUP);
+  }
 
-   if (!WiFi.softAP(ssid, password)) {
+  if (!WiFi.softAP(ssid, password)) {
     log_e("Soft AP creation failed.");
-    while(1);
+    while (1);
   }
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
@@ -45,12 +45,12 @@ void setup() {
 
   Serial.println("Server jestachtet");
 }
-    
-  
+
+
 
 void loop() {
 
- WiFiClient client = server.available();   // listen for incoming clients
+  WiFiClient client = server.available();   // listen for incoming clients
 
   if (client) {                             // if you get a client,
     Serial.println("New Client.");           // print a message out the serial port
@@ -65,91 +65,79 @@ void loop() {
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
 
-          //THIS WILL ALWAYS LOOP UNLESS YOU PUT A FOR LOOP STOPPER @ THE END
-          // This loops thru the buzzer buttons to define which is pressed
+            //THIS WILL ALWAYS LOOP UNLESS YOU PUT A FOR LOOP STOPPER @ THE END
+            // This loops thru the buzzer buttons to define which is pressed
 
-         for (int i; i<3; i++){
+            for (int i; i < 3; i++) {
+              //Press the button to intiate which button chimes in
+              if (digitalRead(gameBTNS[i]) == LOW) {
 
+                // This is the indicator light and buzzer sound confirmation
 
+                digitalWrite(gameLEDS[i], HIGH);
 
-      
-         //Press the button to intiate which button chimes in
+                //Buzzer and light flicker
+                tone(buzzer, 1300, 50);
+                delay(buzzerDelay);
+                digitalWrite(gameLEDS[i], LOW);
+                tone(buzzer, 1300, 50);
+                delay(buzzerDelay);
+                digitalWrite(gameLEDS[i], HIGH);
+                tone(buzzer, 1300, 50);
+                delay(buzzerDelay);
+                digitalWrite(gameLEDS[i], LOW);
+                tone(buzzer, 1300, 50);
+                delay(buzzerDelay);
+                digitalWrite(gameLEDS[i], HIGH);
+                tone(buzzer, 1300, 50);
 
-    if (digitalRead(gameBTNS[i]) == LOW){
-
-            // This is the indicator light and buzzer sound confirmation
-     
-    digitalWrite(gameLEDS[i], HIGH);
-
-                       //Buzzer and light flicker
-    tone(buzzer, 1300, 50);
-    delay(buzzerDelay); 
-    digitalWrite(gameLEDS[i], LOW);
-    tone(buzzer, 1300, 50);
-    delay(buzzerDelay);
-    digitalWrite(gameLEDS[i], HIGH); 
-    tone(buzzer, 1300, 50);
-    delay(buzzerDelay); 
-    digitalWrite(gameLEDS[i], LOW);
-    tone(buzzer, 1300, 50);
-    delay(buzzerDelay); 
-    digitalWrite(gameLEDS[i], HIGH); 
-    tone(buzzer, 1300, 50);
-
-    if (!running) {
-        // Starte die Stoppuhr
-        startTime = millis();
-        running = true;
-        client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println();
-          client.println("<p>gameLEDS[i]</p>");
-          client.println("</body></html>");
-          client.println();
-      } else {
-        // Stoppe die Stoppuhr und zeige die Zeit auf dem Webserver an
-        unsigned long elapsedTime = millis() - startTime;
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println();
-          client.print("<html><body><h1>Elapsed Time: ");
-          client.print(elapsedTime);
-          client.println(" milliseconds</h1></body></html>");
-          // Zusätzliche Informationen hinzufügen
-          client.println("<p>gameLEDS[i]</p>");
-          client.println("</body></html>");
-          client.println();
-          break;
-  }        } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
+                if (!running) {
+                  // Starte die Stoppuhr
+                  startTime = millis();
+                  running = true;
+                  client.println("HTTP/1.1 200 OK");
+                  client.println("Content-Type: text/html");
+                  client.println();
+                  client.println("<p>gameLEDS[i]</p>");
+                  client.println("</body></html>");
+                  client.println();
+                  client.stop();
+                }
+                else {
+                  // Stoppe die Stoppuhr und zeige die Zeit auf dem Webserver an
+                  unsigned long elapsedTime = millis() - startTime;
+                  client.println("HTTP/1.1 200 OK");
+                  client.println("Content-Type: text/html");
+                  client.println();
+                  client.print("<html><body><h1>Elapsed Time: ");
+                  client.print(elapsedTime);
+                  client.println(" milliseconds</h1></body></html>");
+                  // Zusätzliche Informationen hinzufügen
+                  client.println("<p>gameLEDS[i]</p>");
+                  client.println("</body></html>");
+                  client.println();
+                  client.stop();
+                }
+              }
+              else {
+                digitalWrite(gameLEDS[i], LOW);
+              }
+            }
+            // Auto resets after 5 seconds
+            delay(resetDelay);
+            // needs to be manually reset when game show button is depressed
+            //for (;;);
+            Serial.println(gameLEDS[i]);
+            Serial.println(gameBTNS[i]);
           }
-}        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
         }
+        else if (c != '\r') {  // if you got anything else but a carriage return character,
+            currentLine += c;      // add it to the end of the currentLine
         }
+        else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
         }
       }
-}
-          client.stop();
-        }
-        running = false;
-      }
- 
-
-
-
-// Auto resets after 5 seconds
-    delay(resetDelay); 
-// needs to be manually reset when game show button is depressed
-    //for (;;);
-    Serial.println(gameLEDS[i]);
-    Serial.println(gameBTNS[i]);
-
-    
-    }else{
-      
-     digitalWrite(gameLEDS[i], LOW);
-         
     }
   }
 }
